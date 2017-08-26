f010fd0cd2ac43f694d24f12f86519051781d642
Omar <omar2535@gmail.com>
2017-08-26 16:27:37 -0700

    Update myscript.js

--- WikipediaViewer/myscript.js
+++ WikipediaViewer/myscript.js
@@ -1,63 +1,58 @@
 
-
-
 $(document).ready(function(){
-
-  $("#mybutton").click(function(){
-  var namelist = [];
-  var descriptionlist = [];
-  var urllist = [];
-  var myNode = document.getElementById("results");
-  myNode.innerHTML = '';
-  var searchterm = $("#searchbox").val();
-  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchterm + "&callback=?";
-    
-    $.ajax({
-      url: url,
+  //this is the API key -- known as the USERID key in twitch
+  var key = "l41u4yxpbq18z1sutqd9k4ct7oai0m";
+  
+  //For freecodecamp channel
+  var freecodecampstatus= "https://api.twitch.tv/kraken/streams/freecodecamp?client_id=" + key;
+  var freecodecampbio = "https://api.twitch.tv/kraken/users/freecodecamp?client_id=" + key;
+  
+  //For starcraft 2 channel
+  var eslcs2status = "https://api.twitch.tv/kraken/streams/ESL_SC2?client_id=" + key;
+  var eslcs2bio = "https://api.twitch.tv/kraken/users/ESL_SC2?client_id=" + key;
+  
+  //For robotcaleb's channel
+  var robotcalebstatus = "https://api.twitch.tv/kraken/streams/RobotCaleb?client_id=" + key;
+  var robotcalebbio = "https://api.twitch.tv/kraken/users/RobotCaleb?client_id=" + key;
+  
+  //An array for each type of stream URL's
+  
+  var streamstatusurls = [freecodecampstatus, eslcs2status, robotcalebstatus];
+  var streambiourls = [freecodecampbio, eslcs2bio, robotcalebbio];
+  
+  //calling and retrieving status of stream
+  for(var i=0; i<3; i++){
+    (function (i){
+      $.ajax({
+      url: streamstatusurls[i],
       type: "GET",
-      Async: "false",
       dataType: "json",
-      error: function(){
-        alert("failed to retrive API");
-      },
       success: function(data){
-        
-        console.log(data);
-        var length = data[1].length;
-        
-        for(var i=0; i<length; i++){
-          namelist.push(data[1][i]);
-          descriptionlist.push(data[2][i]);
-          urllist.push(data[3][i]);
-        }
-        
-        $("#resultstext").html(descriptionlist[1]);
-        
-        
-        for(var i=0; i<length; i++){
+      
+      console.log(data.stream);
+      console.log(i);
+      
+      if(data.stream != null){
+
+        $("#ch"+i+"-status").html("<strong style = \"color: red\"> Online </strong>");
+        $.ajax({
+          url: streambiourls[i],
+          type: "GET",
+          dataType: "json",
+          success: function(data){
+            $("#ch1-bio").html(data.bio);
+          }
           
-          var p = document.createElement("P");
-         
-          var att = document.createElement("a");
-          var linkText = document.createTextNode(namelist[i]);
-          att.appendChild(linkText);
-          att.href = urllist[i];
-          var a = document.createElement('P');
-          a.innerHTML = "<p>" + descriptionlist[i] + "</p>";
-          p.appendChild(att);
-          p.appendChild(a);
-          document.getElementById("results").appendChild(p);
-          
-          
-        }
+        })
         
       }
-      
-      
-           
-    })
-  
-  
+    }
   })
- 
-});
+    })(i);
+  
+}
+  
+  
+  
+  
+})
